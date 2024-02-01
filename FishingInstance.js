35fb0dc200848c6bfcb9df0e9e2fd7788314d2d1
MaximumG9 <maximeguy9@gmail.com>
2024-01-31 18:31:05 -0800

    Minor Changes

--- FishingInstance.js
+++ FishingInstance.js
@@ -1,86 +1,88 @@
-class FishingInstance {
-    randomUUID() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-            return v.toString(16);
-        });
-    }
+module.exports = {
+Fishing: class FishingClient {
+        randomUUID() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        }
 
-    gamble(bet) {
-        const data = {
-            "username": this.username,
-            "loginKey": this.loginKey,
-            "bet": bet
-        };
-    
-        fetch("http://traoxfish.us-3.evennode.com/gamble", {
+        async gamble(bet) {
+            const data = {
+                "username": this.username,
+                "loginKey": this.loginKey,
+                "bet": bet
+            };
+        
+            return await fetch("http://traoxfish.us-3.evennode.com/gamble", {
+                    method: 'POST',
+                    credentials: "same-origin",
+                    headers: {
+                        accept: 'application/json',
+                    },
+                    body: JSON.stringify(data1),
+                }).then(response => {
+                    return response.json();
+                }).then(json => {
+                    if (json.status == "success") {
+                        return json.winnings;
+                    } else {
+                        return -1;
+                    }
+                })
+        }
+
+        init() {
+            setInterval(this.keepOnline,666);
+        }
+        
+        constructor(username, password) {
+            this.username = username;
+            this.password = password;
+            this.browserKey = this.randomUUID();
+            this.loginKey = getLoginKey(username,password,this.browserKey);
+        }
+
+        getLoginKey(username, password, browserKey) {
+            const data = {
+                "username": username,
+                "password": password,
+                "browserKey": browserKey
+            };
+            fetch('https://traoxfish.us-3.evennode.com/login', {
                 method: 'POST',
                 credentials: "same-origin",
                 headers: {
                     accept: 'application/json',
                 },
-                body: JSON.stringify(data1),
+                body: JSON.stringify(data),
             }).then(response => {
                 return response.json();
             }).then(json => {
                 if (json.status == "success") {
-                    return json.winnings;
+                    loginKey =  json.key;
                 } else {
-                    return -1;
                 }
             });
-    }
+        }
 
-    init() {
-        setInterval(this.keepOnline,666);
-    }
-    
-    constructor(username, password) {
-        this.username = username;
-        this.password = password;
-        this.browserKey = this.randomUUID();
-        this.loginKey = getLoginKey(username,password,this.browserKey);
-    }
-
-    getLoginKey(username, password, browserKey) {
-        const data = {
-            "username": username,
-            "password": password,
-            "browserKey": browserKey
-        };
-        fetch('https://traoxfish.us-3.evennode.com/login', {
-            method: 'POST',
-            credentials: "same-origin",
-            headers: {
-                accept: 'application/json',
-            },
-            body: JSON.stringify(data),
-        }).then(response => {
-            return response.json();
-        }).then(json => {
-            if (json.status == "success") {
-                loginKey =  json.key;
-            } else {
-            }
-        });
-    }
-
-    keepOnline() {
-        const data = {
-            "username": username,
-            "loginKey": loginKey,
-        };
-        fetch('https://traoxfish.us-3.evennode.com/online', {
-            method: 'POST',
-            credentials: "same-origin",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        }).then(response => {
-            return response.json();
-        }).then(json => {
-    
-        });
+        keepOnline() {
+            const data = {
+                "username": username,
+                "loginKey": loginKey,
+            };
+            fetch('https://traoxfish.us-3.evennode.com/online', {
+                method: 'POST',
+                credentials: "same-origin",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }).then(response => {
+                return response.json();
+            }).then(json => {
+        
+            });
+        }
     }
 }
